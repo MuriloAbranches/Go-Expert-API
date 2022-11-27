0413ba8a7c668ff3e90724df686a992ac52b399c
MuriloAbranches <muriloads@hotmail.com>
2022-11-27 20:45:43 -0300

    Initiating Swagger documentation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,16 +5,35 @@ import (
 	"net/http"
 
 	"github.com/MuriloAbranches/Go-Expert-API/configs"
+	_ "github.com/MuriloAbranches/Go-Expert-API/docs"
 	"github.com/MuriloAbranches/Go-Expert-API/internal/entity"
 	"github.com/MuriloAbranches/Go-Expert-API/internal/infra/database"
 	"github.com/MuriloAbranches/Go-Expert-API/internal/webserver/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+// @title 						Go Expert API Example
+// @version 					1.0
+// @description 				Product API with authentication
+// @termsOfService 				http://swagger.io/terms/
+
+// @contact.name 				Murilo Abranches
+// @contact.url 				https://github.com/MuriloAbranches
+// @contact.email 				https://github.com/MuriloAbranches
+
+// @license.name 				MIT
+// @license.url 				https://github.com/MuriloAbranches
+
+// @host 						localhost:8000
+// @BasePath 					/
+// @securityDefinitions.apikey 	ApiKeyAuth
+// @in 							header
+// @name 						Authorization
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
@@ -54,6 +73,8 @@ func main() {
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
